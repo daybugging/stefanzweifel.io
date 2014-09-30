7f1af1111872970dbdbafaaf1d7a191f6dbac3e3
Stefan Zweifel <hello@stefanzweifel.io>
2014-09-30 19:47:56 +0200

    Gulp all the assets!
    Signed-off-by: Stefan Zweifel <hello@stefanzweifel.io>

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    minifyCSS = require('gulp-minify-css');
+
+var paths = {
+    sass: './_dev/sass/'
+}
+
+gulp.task('default', function() {
+    gulp.watch(paths.sass + '*', ['sass']);
+});
+
+
+gulp.task('sass', function () {
+    gulp.src(paths.sass + '*.scss')
+        .pipe(sass())
+        .pipe(minifyCSS( { keepBreaks:false } ))
+        .pipe(gulp.dest('./css'));
+});
