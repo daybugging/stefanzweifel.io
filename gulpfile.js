ea2f03172f18ec1bd0998fb97853488cbf3ddb6d
Stefan Zweifel <hello@stefanzweifel.io>
2014-10-15 20:57:10 +0200

    Added TinyPNG to Gulp Workflow
    Signed-off-by: Stefan Zweifel <hello@stefanzweifel.io>

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,45 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
+/**
+ * Gulp specific Variables
+ */
+var config    = require('./gulpconfig.json'),
+    gulp      = require('gulp'),
+    sass      = require('gulp-sass'),
     minifyCSS = require('gulp-minify-css');
+    tinypng   = require('gulp-tinypng');
 
+/**
+ * Projects paths
+ * @type {Object}
+ */
 var paths = {
     sass: './_dev/sass/'
 }
 
+/**
+ * Default Task
+ * Watch for changes in SASS Files
+ */
 gulp.task('default', function() {
     gulp.watch(paths.sass + '*/*', ['sass']);
     gulp.watch(paths.sass + '*', ['sass']);
 });
 
-
+/**
+ * SASS and Minify Stylesheets
+ */
 gulp.task('sass', function () {
     gulp.src(paths.sass + '*.scss')
         .pipe(sass())
         .pipe(minifyCSS( { keepBreaks:false } ))
         .pipe(gulp.dest('./css'));
+});
+
+/**
+ * Compress PNG Files by tinypng.com API
+ * Define API key in gulpconfig.json
+ */
+gulp.task('png', function () {
+    gulp.src('img/portfolio/**/*.png')
+        .pipe(tinypng( config.tinypng_api_key ))
+        .pipe(gulp.dest('compressed_images'));
 });
