75eb5ad23d30205656d422d435cf747024c4cb43
Stefan Zweifel <hello@stefanzweifel.io>
2016-05-07 22:41:47 +0200

    build.js is now a Metalsmith Process again
    Signed-off-by: Stefan Zweifel <hello@stefanzweifel.io>

--- scripts/metalsmith/build.js
+++ scripts/metalsmith/build.js
@@ -1,6 +1,6 @@
-var gulp               = require('gulp');
+var Metalsmith         = require('Metalsmith');
 var gulpsmith          = require('gulpsmith');
-var frontMatter  = require('gulp-front-matter');
+var frontMatter        = require('gulp-front-matter');
 var assign             = require('lodash.assign');
 var assets             = require('metalsmith-assets');
 var drafts             = require('metalsmith-drafts');
@@ -20,60 +20,50 @@ var define             = require('metalsmith-define');
 var excerpts           = require('metalsmith-better-excerpts');
 var excerpts           = require('metalsmith-excerpts');
 
+Metalsmith('./')
+    .metadata(require('./metadata.js'))
+    .use(define({
+        moment: require('moment')
+    }))
+    .use(require('./collections.js'))
+    .use(require('./markdown.js'))
+    .use(require('./permalinks.js'))
+    .use(drafts())
+    .use(ignore(['layouts/**/*', 'partials/**/*']))
+    .use(pageTitles())
+    .use(wordcount())
+    .use(images({
+        pattern: '**/**/*.md'
+    }))
 
-module.exports = function() {
+    .use(excerpts({
+        pruneLength: 160
+    }))
 
-    // gulp.src(["./src/**/*"])
-    gulp.src(["./src/**/*.{md,html,css,xml,txt}"])
-        .pipe(frontMatter()).on("data", function(file) {
-
-            // This thing fucks up with my images. And I can't stop it :(
-            assign(file, file.frontMatter);
-            delete file.frontMatter;
-        })
-        .pipe(
-            gulpsmith()
-                .metadata(require('./metadata.js'))
-                .use(define({
-                    moment: require('moment')
-                }))
-                .use(require('./collections.js'))
-                .use(require('./markdown.js'))
-                .use(require('./permalinks.js'))
-                .use(drafts())
-                .use(ignore(['layouts/**/*', 'partials/**/*']))
-                .use(pageTitles())
-                .use(wordcount())
-                .use(images({
-                    pattern: '**/**/*.md'
-                }))
-
-                .use(excerpts({
-                    pruneLength: 160
-                }))
-
-                // Experimental Stuff ------------------------------------------
-                // .use(assets({
-                //     source: './src/assets', // relative to the working directory
-                //     destination: './assets' // relative to the build directory
-                // }))
-                // .use(hyphenate({
-                //     "elements": ["p", "blockquote"],
-                //     "langModule": "hyphenation.de",
-                //     "ignore": ["archives/*"] // All the files inside 'archive' will not be hyphenated
-                // }))
-                // .use(metallic()) // highlight.js
-                // .use(feed({
-                //     collection: 'articles',
-                //     destination: 'feed.xml'
-                // }))
-                .use(sitemap('https://stefanzweifel.io'))
-                // ----------------------------------------------------------
-                .use(require('./layouts.js'))
-                .use(require('./inPlace.js'))
-                .use(headingsidentifier())
-                // .use(htmlMinifier()) // Use the default options
-                // .use(linkcheck())
-        )
-        .pipe(gulp.dest("./dist"));
-};
+    // Experimental Stuff ------------------------------------------
+    // .use(assets({
+    //     source: './src/assets', // relative to the working directory
+    //     destination: './assets' // relative to the build directory
+    // }))
+    // .use(hyphenate({
+    //     "elements": ["p", "blockquote"],
+    //     "langModule": "hyphenation.de",
+    //     "ignore": ["archives/*"] // All the files inside 'archive' will not be hyphenated
+    // }))
+    .use(metallic()) // highlight.js
+    // .use(feed({
+    //     collection: 'articles',
+    //     destination: 'feed.xml'
+    // }))
+    .use(sitemap(require('./metadata.js').site.url))
+    // ----------------------------------------------------------
+    .use(require('./layouts.js'))
+    .use(require('./inPlace.js'))
+    .use(headingsidentifier())
+    // .use(htmlMinifier()) // Use the default options
+    // .use(linkcheck())
+    .destination('./dist')
+    .build(function(err) {
+    if (err) throw err;
+    console.log('Build finished!');
+    });
