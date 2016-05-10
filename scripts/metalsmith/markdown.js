78d4dbfcf9b97de67b5e32a22b94591da0be640a
Stefan Zweifel <hello@stefanzweifel.io>
2016-05-10 21:51:12 +0200

    Switch Markdown Package to "Remarkable"
    Signed-off-by: Stefan Zweifel <hello@stefanzweifel.io>

--- scripts/metalsmith/markdown.js
+++ scripts/metalsmith/markdown.js
@@ -1,8 +1,23 @@
-var markdown = require('metalsmith-markdown');
+var markdown = require('metalsmith-markdown-remarkable');
+var hljs     = require('highlight.js');
 
-module.exports = markdown({
-    smartypants: true,
-    gfm: true,
-    tables: true,
-    footnotes: true
-});
+module.exports = markdown('full', {
+    breaks: true,
+    html: true,
+    linkify: true,
+    langPrefix: 'hljs language-',
+    typographer: true,
+    highlight: function (str, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+            try {
+                return hljs.highlight(lang, str).value;
+            } catch (err) {}
+        }
+
+        try {
+            return hljs.highlightAuto(str).value;
+        } catch (err) {}
+
+        return ''; // use external default escaping
+    }
+})
