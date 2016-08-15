c49573f73c4ccc0cf800725584bd95e324169d8d
Stefan Zweifel <hello@stefanzweifel.io>
2016-08-15 22:30:35 +0200

    Update textlint Rules

--- scripts/textlint.js
+++ scripts/textlint.js
@@ -3,7 +3,14 @@ import textlint from 'gulp-textlint';
 
 export default () =>  {
 
-    return gulp.src('./src/**/*.md')
+    let files = [
+        // './src/articles/2016/*.md',
+        './src/projects/**/*.md',
+        './src/pages/**/*.md'
+    ];
+
+
+    return gulp.src(files)
         .pipe(textlint({
             formatterName: "pretty-error"
         }));
